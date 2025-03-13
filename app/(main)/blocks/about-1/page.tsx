139999f412f81fef284e48e9bfa87dfdb4486a69
Ali Abdelhadi <aliabdelhadi010@gmail.com>
2025-03-13 02:00:43 +0200

    update project

--- app/(main)/blocks/about-1/page.tsx
+++ app/(main)/blocks/about-1/page.tsx
@@ -1,6 +1,9 @@
 import About_1 from "@/components/blocks/about-1/about-1";
+
 const About_1Page = () => {
-  return <About_1 />;
+  return (
+    <About_1 />
+  );
 };
 
 export default About_1Page;
